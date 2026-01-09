58eb1d5354412decb7da955d45bf153660cff21e
neducod <nedunony16@gmail.com>
2026-01-09 19:51:09 +0100

    last commit for the day, created a script file and cleaned up my code

---
+++ script.js
@@ -0,0 +1,10 @@
+            //ScrollTrigger Animation
+            gsap.from(".section", {
+                opacity: 0,
+                y: 100,
+                duration: 1,
+                scrollTrigger: {
+                    trigger: ".section",
+                    start: "top 80%",
+                }
+            });
